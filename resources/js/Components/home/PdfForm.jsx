b38f8e2396363a89a57b0c5959640572b9d76e9e
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2024-06-24 15:43:59 +0600

    added all plugins into form

--- resources/js/Components/home/PdfForm.jsx
+++ resources/js/Components/home/PdfForm.jsx
@@ -30,6 +30,7 @@ const PdfForm = ({ template: dbTemplate }) => {
                             theme: {},
                         },
                         inputs,
+                        plugins: getPlugins(),
                     });
                 }
             });
