e2721fcda142f0a3fe739f8bec433db8749f725d
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2024-09-07 16:23:44 +0600

    updated smtp

--- app/Http/Controllers/SmtpSettingController.php
+++ app/Http/Controllers/SmtpSettingController.php
@@ -11,16 +11,16 @@ use Inertia\Inertia;
 
 class SmtpSettingController extends Controller
 {
-    public function getSmtpConfigView(){
-        $smtp_configs=SmtpSetting::get();
-        return Inertia::render('Admin/EmailTemplates/SmtpSettings',[
-            "smtp_settings"=>$smtp_configs
+    public function getSmtpConfigView()
+    {
+        $smtp_configs = SmtpSetting::get();
+        return Inertia::render('Admin/EmailTemplates/SmtpSettings', [
+            "smtp_settings" => $smtp_configs
         ]);
-       
     }
     public function create(Request $request)
     {
-        try{
+        try {
             $request->validate([
                 'mail_host' => 'required|string',
                 'mail_port' => 'required|integer',
@@ -31,17 +31,17 @@ class SmtpSettingController extends Controller
                 'mail_from_name' => 'nullable|string',
                 'status' => 'required|in:active,inactive',
             ]);
-             $this->makeOtherConfigDeactivated($request->status);
-            $smtp_settings=SmtpSetting::create($request->all());
-            if($request->status==="active"){
+            $this->makeOtherConfigDeactivated($request->status);
+            $smtp_settings = SmtpSetting::create($request->all());
+            if ($request->status === "active") {
                 $this->setMailConfig($smtp_settings);
             }
             return response()->json([
-                "message"=>"Smtp successfully created",
-                "success"=>true,
-                "smtpSettings"=>$smtp_settings
+                "message" => "Smtp successfully created",
+                "success" => true,
+                "smtpSettings" => $smtp_settings
             ]);
-        }catch (ValidationException $e) {
+        } catch (ValidationException $e) {
             return response()->json([
                 "message" => "Validation failed",
                 "errors" => $e->errors(),
@@ -49,9 +49,9 @@ class SmtpSettingController extends Controller
             ], 422);
         }
     }
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
-        try{
+        try {
             $request->validate([
                 'mail_host' => 'required|string',
                 'mail_port' => 'required|integer',
@@ -64,23 +64,23 @@ class SmtpSettingController extends Controller
             ]);
             $settings = SmtpSetting::find($id);
             if (!$settings) {
-              return response()->json([
-                "message"=>"Smtp config not found",
-                "success"=>false,
-            ]);
-            } 
+                return response()->json([
+                    "message" => "Smtp config not found",
+                    "success" => false,
+                ]);
+            }
             $this->makeOtherConfigDeactivated($request->status);
             $settings->update($request->all());
 
-            if($request->status==="active"){
+            if ($request->status === "active") {
                 $this->setMailConfig($settings);
             }
             return response()->json([
-                "message"=>"Smtp successfully created",
-                "success"=>true,
-                "smtpSettings"=>$settings
+                "message" => "Smtp successfully created",
+                "success" => true,
+                "smtpSettings" => $settings
             ]);
-        }catch (ValidationException $e) {
+        } catch (ValidationException $e) {
             return response()->json([
                 "message" => "Validation failed",
                 "errors" => $e->errors(),
@@ -89,8 +89,9 @@ class SmtpSettingController extends Controller
         }
     }
 
-    private function makeOtherConfigDeactivated($status){
-         if ($status === 'active') {
+    private function makeOtherConfigDeactivated($status)
+    {
+        if ($status === 'active') {
             // Set all other SMTP settings to inactive
             SmtpSetting::where('status', 'active')->update(['status' => 'inactive']);
         }
@@ -106,15 +107,15 @@ class SmtpSettingController extends Controller
         Config::set('mail.from.address', $smtpSetting->mail_from_address);
         Config::set('mail.from.name', $smtpSetting->mail_from_name);
 
-           Log::info('Mail Config:', [
-        'transport' => Config::get('mail.mailers.smtp.transport'),
-        'host' => Config::get('mail.mailers.smtp.host'),
-        'port' => Config::get('mail.mailers.smtp.port'),
-        'username' => Config::get('mail.mailers.smtp.username'),
-        'password' => Config::get('mail.mailers.smtp.password'),
-        'encryption' => Config::get('mail.mailers.smtp.encryption'),
-        'from_address' => Config::get('mail.from.address'),
-        'from_name' => Config::get('mail.from.name'),
-    ]);
+        Log::info('Mail Config:', [
+            'transport' => Config::get('mail.mailers.smtp.transport'),
+            'host' => Config::get('mail.mailers.smtp.host'),
+            'port' => Config::get('mail.mailers.smtp.port'),
+            'username' => Config::get('mail.mailers.smtp.username'),
+            'password' => Config::get('mail.mailers.smtp.password'),
+            'encryption' => Config::get('mail.mailers.smtp.encryption'),
+            'from_address' => Config::get('mail.from.address'),
+            'from_name' => Config::get('mail.from.name'),
+        ]);
     }
 }
